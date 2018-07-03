161945ef25db2501b638b43cce8017408121d4e4
Rebecca McLaughlin <daelune@gmail.com>
2018-07-03 18:55:44 +0100

    Enemies to chase player

--- Assets/Enemies/Enemy.cs
+++ Assets/Enemies/Enemy.cs
@@ -1,18 +1,42 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
 
-public class Enemy : MonoBehaviour {
+public class Enemy : MonoBehaviour
+{
 
     [SerializeField] float maxHealthPoints = 100f;
+    [SerializeField] float attackRadius = 5f;
 
     float currentHealthPoints = 100f;
+    AICharacterControl aiCharacterControl = null;
+    GameObject target = null;
 
-	public float healthAsPercentage
+    public float healthAsPercentage
     {
         get
         {
             return currentHealthPoints / maxHealthPoints;
+        }
+    }
+
+    public void Start()
+    {
+        aiCharacterControl = GetComponent<AICharacterControl>();
+        target = GameObject.FindGameObjectWithTag("Player");
+        //target = aiCharacterControl.target;
+    }
+
+    public void Update()
+    {
+
+        float distanceToPlayer = Vector3.Distance(target.transform.position, transform.position);
+        if (distanceToPlayer <= attackRadius)
+        {
+            aiCharacterControl.SetTarget(target.transform);
+        }
+        else
+        {
+            aiCharacterControl.SetTarget(transform);
         }
     }
 }
